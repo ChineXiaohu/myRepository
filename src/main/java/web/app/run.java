9d075f1e8463053b9b383f21cbc6726adc0e0f1a
Petter <Petter@192.168.0.161>
2018-08-15 19:21:43 +0800

    int1

--- src/main/java/web/app/run.java
+++
@@ -1,8 +0,0 @@
-package web.app;
-
-public class run {
-public static void main(String[] args) {
-	System.out.println("imrun");
-	System.out.println();
-}
-}
--- src/main/java/web/app/test.java
+++ src/main/java/web/app/test.java
@@ -3,7 +3,6 @@ package web.app;
 public class test {
 public static void main(String[] args) {
 	System.out.println("你好");
-	System.out.println("我很好");
-	System.out.println("啦啦啦");
+
 }
 }
